3320344a9c28ad6c5fc5d7ffb50740b769bb5246
Kyle <kylekmatthew15@gmail.com>
2024-03-29 19:17:14 -0400

    fixed the playRound function

--- index.js
+++ index.js
@@ -4,30 +4,28 @@ function getComputerChoice() {
 }
 
 function playerChoice() {
-
     let userInput = prompt("Rock, Paper or Scissors");
     userInput = userInput.toUpperCase();
     return userInput;
 }
 
 function playRound() {
-let playerSelection = playerChoice();
-let computerSelection = getComputerChoice(); 
+    let playerSelection = playerChoice();
+    let computerSelection = getComputerChoice(); 
 
-if(playerSelection === computerSelection) {
-    console.log("You draw !");
-} else if (playerSelection === "ROCK" && computerSelection === "Scissors") {
-    console.log(`You win ${playerSelection} beats ${computerSelection}`);
-} else if (playerSelection === "PAPER" && computerSelection === "Rock") {
-    console.log(`You win ${playerSelection} beats ${computerSelection}`);
-} else if (playerSelection === "SCISSORS" && computerSelection === "Paper") {
-    console.log(`You win ${playerSelection} beats ${computerSelection}`);
-} else if (playerSelection === "ROCK" && computerSelection === "Paper") {
-    console.log(`You lose ${computerSelection} beats ${playerSelection}`);
-} else if (playerSelection === "PAPER" && computerSelection === "Scissors") {
-    console.log(`You lose ${computerSelection} beats ${playerSelection}`);
-} else if (playerSelection === "SCISSORS" && computerSelection === "Rock") {
-    console.log(`You lose ${computerSelection} beats ${playerSelection}`);
-}
-
+    if(playerSelection === computerSelection) {
+        console.log("You draw !");
+    } else if (playerSelection === "ROCK" && computerSelection === "Scissors") {
+        console.log(`You win ${playerSelection} beats ${computerSelection}`);
+    } else if (playerSelection === "PAPER" && computerSelection === "Rock") {
+        console.log(`You win ${playerSelection} beats ${computerSelection}`);
+    } else if (playerSelection === "SCISSORS" && computerSelection === "Paper") {
+        console.log(`You win ${playerSelection} beats ${computerSelection}`);
+    } else if (playerSelection === "ROCK" && computerSelection === "Paper") {
+        console.log(`You lose ${computerSelection} beats ${playerSelection}`);
+    } else if (playerSelection === "PAPER" && computerSelection === "Scissors") {
+        console.log(`You lose ${computerSelection} beats ${playerSelection}`);
+    } else if (playerSelection === "SCISSORS" && computerSelection === "Rock") {
+        console.log(`You lose ${computerSelection} beats ${playerSelection}`);
+    }
 }

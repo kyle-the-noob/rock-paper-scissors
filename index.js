ac551c23e0fea544f9e7f263dbbef61f88a759eb
Kyle <kylekmatthew15@gmail.com>
2024-04-16 19:10:32 -0400

    Added event listeners to the buttons

--- index.js
+++ index.js
@@ -1,40 +1,64 @@
 function getComputerChoice() {
-    let choiceArray = ['Rock', 'Paper', 'Scissors'];
-    return choice = choiceArray[Math.floor(Math.random() * choiceArray.length)];
-}
+        let choiceArray = ['Rock', 'Paper', 'Scissors'];
+        return choice = choiceArray[Math.floor(Math.random() * choiceArray.length)];
+    }
 
-function playerChoice() {
-    let userInput = prompt("Rock, Paper or Scissors");
-    userInput = userInput.toUpperCase();
-    return userInput;
+function playerChoice(buttons) {
+
+        /*let userInput = prompt("Rock, Paper or Scissors");
+        userInput = userInput.toUpperCase();
+        return userInput;*/
+
+
 }
 
 function playRound() {
-    let playerSelection = playerChoice();
+    /*let playerSelection = playerChoice();*/
     let computerSelection = getComputerChoice(); 
 
     if(playerSelection === computerSelection) {
         console.log("You draw !");
-    } else if (playerSelection === "ROCK" && computerSelection === "Scissors") {
+    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
         console.log(`You win ${playerSelection} beats ${computerSelection}`);
-    } else if (playerSelection === "PAPER" && computerSelection === "Rock") {
+    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
         console.log(`You win ${playerSelection} beats ${computerSelection}`);
-    } else if (playerSelection === "SCISSORS" && computerSelection === "Paper") {
+    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
         console.log(`You win ${playerSelection} beats ${computerSelection}`);
-    } else if (playerSelection === "ROCK" && computerSelection === "Paper") {
+    } else if (playerSelection === "Rock" && computerSelection === "Paper") {
         console.log(`You lose ${computerSelection} beats ${playerSelection}`);
-    } else if (playerSelection === "PAPER" && computerSelection === "Scissors") {
+    } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
         console.log(`You lose ${computerSelection} beats ${playerSelection}`);
-    } else if (playerSelection === "SCISSORS" && computerSelection === "Rock") {
+    } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
         console.log(`You lose ${computerSelection} beats ${playerSelection}`);
     }
 }
 
-function playGame() {
+let playerSelection;
+
+/*function playGame() {
     let counter = 1;
 
     while (counter <= 5) {
         playRound();
         counter++;
     }
-}
+}*/
+
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll("button");
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener("click", () => {
+    if (button == rock) {
+        playerSelection = "Rock";
+        playRound();
+    } else if(button === paper) {
+        playerSelection = "Paper";
+        playRound();
+    } else {
+        playerSelection = "Scissors";
+        playRound();
+    }
+  });
+});

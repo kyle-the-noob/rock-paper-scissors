6ef3865137cf2d50e1db9277612f016abbe6c4bf
Kyle <kylekmatthew15@gmail.com>
2024-04-17 10:55:22 -0400

    Made the running score and winnermsg functional as well as reset page if winner conditions are met

--- index.js
+++ index.js
@@ -1,28 +1,50 @@
 function getComputerChoice() {
-        let choiceArray = ['Rock', 'Paper', 'Scissors'];
-        return choice = choiceArray[Math.floor(Math.random() * choiceArray.length)];
+    let choiceArray = ['Rock', 'Paper', 'Scissors'];
+    return choice = choiceArray[Math.floor(Math.random() * choiceArray.length)];
     }
 
-function playRound() {
+function playGame() {
     let computerSelection = getComputerChoice(); 
     const div = document.querySelector("div");
+    const score = document.getElementById("score");
+    const winnermsg = document.getElementById("winnermsg");
 
-    if(playerSelection === computerSelection) {
-        div.textContent = "You draw !"
-    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
-        div.textContent = `You win ${playerSelection} beats ${computerSelection}`
-    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
-        div.textContent = `You win ${playerSelection} beats ${computerSelection}`
-    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
-        div.textContent = `You win ${playerSelection} beats ${computerSelection}`
-    } else if (playerSelection === "Rock" && computerSelection === "Paper") {
-        div.textContent = `You lose ${computerSelection} beats ${playerSelection}`
-    } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
-        div.textContent = `You lose ${computerSelection} beats ${playerSelection}`
-    } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
-        div.textContent = `You lose ${computerSelection} beats ${playerSelection}`
+    for (let i = 0; i < 1; i++) { 
+    if (playerSelection === "Rock" && computerSelection === "Scissors" || 
+        playerSelection === "Paper" && computerSelection === "Rock" || 
+        playerSelection === "Scissors" && computerSelection === "Paper") {
+        div.textContent = `You win ${playerSelection} beats ${computerSelection}`;
+        playerScore ++;
+    } else if (playerSelection === "Rock" && computerSelection === "Paper" ||
+        playerSelection === "Paper" && computerSelection === "Scissors" ||
+        playerSelection === "Scissors" && computerSelection === "Rock") {
+        div.textContent = `You lose ${computerSelection} beats ${playerSelection}`;
+        cpuScore ++;
+    } else {
+        div.textContent = "You draw !";
+    } score.textContent = (`Score ${playerScore} : ${cpuScore}`);
+}
+    function checkForWin() {
+        if (playerScore === 5){
+            winnermsg.textContent = "You win!";
+            gameEnd = true;
+        } else if ( cpuScore === 5) {
+            winnermsg.textContent = "The computer wins!";
+            gameEnd = true;
+        }
+}
+checkForWin();
+
+    if(gameEnd === true) {
+        playerScore = 0;
+        cpuScore = 0;
+        gameEnd = false;
     }
 }
+
+let gameEnd = false;
+let cpuScore = 0;
+let playerScore = 0;
 
 let playerSelection;
 const buttons = document.querySelectorAll("button");
@@ -31,23 +53,13 @@ buttons.forEach((button) => {
   button.addEventListener("click", () => {
     if (button == rock) {
         playerSelection = "Rock";
-        playRound();
+        playGame();
     } else if(button === paper) {
         playerSelection = "Paper";
-        playRound();
+        playGame();
     } else {
         playerSelection = "Scissors";
-        playRound();
+        playGame();
     }
   });
 });
-
-/*function playGame() {
-    let counter = 1;
-
-    while (counter <= 5) {
-        playRound();
-        counter++;
-    }
-}*/
-
